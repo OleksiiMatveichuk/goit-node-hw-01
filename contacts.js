615bd625816352fce69a30a5383321403af5ab7d
OleksiiMatveichuk <126767121+OleksiiMatveichuk@users.noreply.github.com>
2023-07-19 11:03:01 +0300

    fix and add gitignore

--- contacts.js
+++ contacts.js
@@ -2,13 +2,13 @@ const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
 
-const contactsPath = path.join(__dirname, "db/contacts.json");
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 const updateContacts = async (contacts) =>
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
 const listContacts = async () => {
-  const data = await fs.readFile(contactsPath);
+  const data = await fs.readFile(contactsPath, "utf-8");
   return JSON.parse(data);
 };
 
